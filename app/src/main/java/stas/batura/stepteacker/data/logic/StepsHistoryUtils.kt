73ea1792064f6fef72c380fb3d3ea9884b361d17
Note <seeyou85@gmail.com>
2021-11-17 00:43:18 +0300

    добавлены тесты

--- app/src/main/java/stas/batura/stepteacker/data/logic/StepsHistoryUtils.kt
+++ app/src/main/java/stas/batura/stepteacker/data/logic/StepsHistoryUtils.kt
@@ -6,15 +6,19 @@ import stas.batura.stepteacker.data.room.Step
  * Вычисление количество шагов за день по списку записей Steps из Бд.
  */
 fun getStepsInDay(steps: List<Step>): Int {
-    val stepsDel = arrayOfNulls<Int?>(steps.size-1)
-    for (i in 0 until steps.size-1) {
-        stepsDel[i] = steps[i+1].steps - steps[i].steps
-    }
-    var sum = 0
-    for (del in stepsDel) {
-        if (del != null && del > 0) {
-            sum = sum + del
+    if (steps.size > 1) {
+        val stepsDel = arrayOfNulls<Int?>(steps.size - 1)
+        for (i in 0 until steps.size - 1) {
+            stepsDel[i] = steps[i + 1].steps - steps[i].steps
         }
+        var sum = 0
+        for (del in stepsDel) {
+            if (del != null && del > 0) {
+                sum = sum + del
+            }
+        }
+        return sum
+    } else {
+        return 0
     }
-    return sum
 }
--- app/src/test/java/stas/batura/stepteacker/steps/DaysCalculationTests.kt
+++ app/src/test/java/stas/batura/stepteacker/steps/DaysCalculationTests.kt
@@ -46,4 +46,64 @@ class DaysCalculationTests {
         assertEquals(1+6+1+3+1, getStepsInDay(steps))
     }
 
+    @Test
+    fun steps_byPlusFrom_from111_big_count_isCorrect() {
+        val steps = mutableListOf<Step>(
+            Step(steps = 111),
+            Step(steps = 213),
+            )
+        assertEquals(102, getStepsInDay(steps))
+    }
+
+    @Test
+    fun steps_byPlusFrom_from111_count_isCorrect() {
+        val steps = mutableListOf<Step>(
+            Step(steps = 111),
+            Step(steps = 113),
+            Step(steps = 119),
+            Step(steps = 129),
+
+        )
+        assertEquals(2+6+10, getStepsInDay(steps))
+    }
+
+    @Test
+    fun steps_one_record_count_isCorrect() {
+        val steps = mutableListOf<Step>(
+            Step(steps = 111),
+        )
+        assertEquals(0, getStepsInDay(steps))
+    }
+
+    @Test
+    fun steps_record_two_intervals_count_isCorrect() {
+        val steps = mutableListOf<Step>(
+            Step(steps = 0),
+            Step(steps = 1),
+            Step(steps = 7),
+            Step(steps = 8),
+
+            Step(steps = 0),
+            Step(steps = 4),
+            Step(steps = 5),
+            Step(steps = 6),
+        )
+        assertEquals(8 + 6, getStepsInDay(steps))
+    }
+
+//    @Test
+//    fun steps_byPlusFrom_from111_count_isCorrect() {
+//        val steps = mutableListOf<Step>(
+//            Step(steps = 0),
+//            Step(steps = 1),
+//            Step(steps = 7),
+//            Step(steps = 8),
+//
+//            Step(steps = 0),
+//            Step(steps = 4),
+//            Step(steps = 5),
+//            Step(steps = 6),
+//            )
+//        assertEquals(2+6+10, getStepsInDay(steps))
+//    }
 }
