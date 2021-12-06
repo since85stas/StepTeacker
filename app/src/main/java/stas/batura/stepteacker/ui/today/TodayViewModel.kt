b569eb16d822a849a1acd59359dbb7a7c11b5eb3
Note <seeyou85@gmail.com>
2021-12-06 23:31:40 +0300

    добавлен viewmodelScope

--- app/src/main/java/stas/batura/stepteacker/ui/today/TodayViewModel.kt
+++ app/src/main/java/stas/batura/stepteacker/ui/today/TodayViewModel.kt
@@ -3,6 +3,9 @@ package stas.batura.stepteacker.ui.today
 import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.flow.flatMapLatest
 import kotlinx.coroutines.flow.map
 import stas.batura.stepteacker.data.Repository
@@ -18,11 +21,22 @@ class TodayViewModel @ViewModelInject constructor(
 
     private val dayhistory = repository.getDaysList(getTimeFormatString(Calendar.getInstance()))
 
+    val viewModelJob = Job()
+    val viewModelScope = CoroutineScope(Dispatchers.IO + viewModelJob)
+
     val stepsForDayFromList = dayhistory.map {
         getStepsInDay(it)
     }.asLiveData()
 
     fun dropStepsTabale() {
         repository.dropStepsTable()
+    }
+
+    /**
+     * Cancel all coroutines when the ViewModel is cleared
+     */
+    override fun onCleared() {
+        super.onCleared()
+        viewModelJob.cancel()
     }
 }
