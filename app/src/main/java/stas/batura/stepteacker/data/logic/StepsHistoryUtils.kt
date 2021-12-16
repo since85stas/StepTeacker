4dd0ce1c794ee460449726f018c7172e93f6da21
Note <seeyou85@gmail.com>
2021-12-16 18:03:06 +0300

    функция для нахождения непрерывной последовательности

--- app/src/main/java/stas/batura/stepteacker/data/logic/StepsHistoryUtils.kt
+++ app/src/main/java/stas/batura/stepteacker/data/logic/StepsHistoryUtils.kt
@@ -3,7 +3,7 @@ package stas.batura.stepteacker.data.logic
 import stas.batura.stepteacker.data.room.Step
 
 /**
- * Вычисление количество шагов за день по списку записей Steps из Бд.
+ * Вычисление количества шагов за день по списку записей Steps из Бд.
  */
 fun getStepsInDay(steps: List<Step>): Int {
     if (steps.size > 1) {
@@ -20,5 +20,30 @@ fun getStepsInDay(steps: List<Step>): Int {
         return sum
     } else {
         return 0
+    }
+}
+
+/**
+ * Вычисление непрерывного распределения изменения шагов по Steps из Бд.
+ */
+fun getStepsSequenceInDay(steps: List<Step>): Array<Int?>? {
+    if (steps.size > 1) {
+        val stepsDel = arrayOfNulls<Int?>(steps.size - 1)
+        for (i in 0 until steps.size - 1) {
+            if (steps[i+1].steps > steps[i].steps) {
+                stepsDel[i] = steps[i + 1].steps - steps[i].steps
+            } else {
+                stepsDel[i] = stepsDel[i-1]!! + steps[i+1].steps
+            }
+        }
+        var sum = 0
+        for (del in stepsDel) {
+            if (del != null && del > 0) {
+                sum = sum + del
+            }
+        }
+        return stepsDel
+    } else {
+        return null
     }
 }
