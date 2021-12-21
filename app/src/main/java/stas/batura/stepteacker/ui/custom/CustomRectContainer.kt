781c15b596a99c8d592a04e93d49589c93c414f1
Note <seeyou85@gmail.com>
2021-12-21 19:12:50 +0300

    добавлено рисование

--- app/src/main/java/stas/batura/stepteacker/ui/custom/CustomRectContainer.kt
+++ app/src/main/java/stas/batura/stepteacker/ui/custom/CustomRectContainer.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
+import android.graphics.RectF
 import android.util.AttributeSet
 import android.view.View
 
@@ -27,6 +28,52 @@ class CustomRectContainer(context: Context, attrs: AttributeSet): View(context, 
     override fun onDraw(canvas: Canvas) {
         // call the super method to keep any drawing from the parent side.
         super.onDraw(canvas)
+
+        drawFaceBackground(canvas)
+        drawEyes(canvas)
+        drawMouth(canvas)
+    }
+
+
+    private fun drawFaceBackground(canvas: Canvas) {
+        // 1
+        paint.color = faceColor
+        paint.style = Paint.Style.FILL
+
+        // 2
+        val radius = size / 2f
+
+        // 3
+        canvas.drawCircle(size / 2f, size / 2f, radius, paint)
+
+        // 4
+        paint.color = borderColor
+        paint.style = Paint.Style.STROKE
+        paint.strokeWidth = borderWidth
+
+        // 5
+        canvas.drawCircle(size / 2f, size / 2f, radius - borderWidth / 2f, paint)
+
+    }
+
+    private fun drawEyes(canvas: Canvas) {
+        // 1
+        paint.color = eyesColor
+        paint.style = Paint.Style.FILL
+
+        // 2
+        val leftEyeRect = RectF(size * 0.32f, size * 0.23f, size * 0.43f, size * 0.50f)
+
+        canvas.drawOval(leftEyeRect, paint)
+
+        // 3
+        val rightEyeRect = RectF(size * 0.57f, size * 0.23f, size * 0.68f, size * 0.50f)
+
+        canvas.drawOval(rightEyeRect, paint)
+    }
+
+    private fun drawMouth(canvas: Canvas) {
+
     }
 
 }
