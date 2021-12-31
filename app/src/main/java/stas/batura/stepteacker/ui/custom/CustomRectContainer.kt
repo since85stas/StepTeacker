8bb8f8d0be6fad40b5f46b4593b978f61fd0ec7a
Note <seeyou85@gmail.com>
2021-12-31 09:55:43 +0300

    заменена отрисовка

--- app/src/main/java/stas/batura/stepteacker/ui/custom/CustomRectContainer.kt
+++ app/src/main/java/stas/batura/stepteacker/ui/custom/CustomRectContainer.kt
@@ -7,17 +7,22 @@ import android.view.View
 
 class CustomRectContainer(context: Context, attrs: AttributeSet): View(context, attrs) {
 
+    companion object {
+        private const val DEFAULT_FILL_COLOR = Color.YELLOW
+        private const val DEFAULT_BORDER_COLOR = Color.BLACK
+        private const val DEFAULT_STEP_LIMIT = 10000
+        private const val DEFAULT_CURRENT_STEPS = 1000
+    }
+
     // Paint object for coloring and styling
     private val paint = Paint(Paint.ANTI_ALIAS_FLAG)
 
     // Some colors for the face background, eyes and mouth.
-    private var bordOuterColor = Color.BLACK
-    private var eyesColor = Color.BLACK
-    private var mouthColor = Color.BLACK
-    private var bordInnerColor = Color.RED
+    private var bordOuterColor = DEFAULT_BORDER_COLOR
+    private var fillerColor = DEFAULT_FILL_COLOR
 
     // Face border width in pixels
-    private var borderWidth = 10
+    private var borderWidth = 2*0.01f
 
     // View size in pixels
     private var size = 320
@@ -43,22 +48,14 @@ class CustomRectContainer(context: Context, attrs: AttributeSet): View(context, 
         // 1
         paint.color = bordOuterColor
         paint.style = Paint.Style.STROKE
-
+        paint.strokeWidth = width*borderWidth
         // 2
         val radius = size / 2f
 
         // 3
 //        canvas.drawCircle(size / 2f, size / 2f, radius, paint)
-        val rectOuter = Rect(5,5,size,size)
+        val rectOuter = Rect(0,0,size,size)
         canvas.drawRect(rectOuter, paint)
-
-        // 4
-        paint.color = bordInnerColor
-        paint.style = Paint.Style.STROKE
-        paint.strokeWidth = borderWidth*1.0f
-
-        val rectInner = Rect(15+borderWidth,15+borderWidth,size-borderWidth,size-borderWidth)
-        canvas.drawRect(rectInner, paint)
 
         // 5
 //        canvas.drawCircle(size*1.0f, size*1.0f, radius *2, paint)
@@ -67,7 +64,6 @@ class CustomRectContainer(context: Context, attrs: AttributeSet): View(context, 
 
     private fun drawEyes(canvas: Canvas) {
         // 1
-        paint.color = eyesColor
         paint.style = Paint.Style.FILL
 
         // 2
